8afd6c3fce42ac70a6b408686863b6b913088f77
Your Namevinicius <viniciusFortti>
2019-07-02 14:53:28 -0300

    (feat):Controller parcial de arquivos.

--- ftpserver/src/main/java/desafio/ftp/ftpserver/Controller/ArquivoController.java
+++ ftpserver/src/main/java/desafio/ftp/ftpserver/Controller/ArquivoController.java
@@ -1,4 +1,19 @@
 package desafio.ftp.ftpserver.Controller;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(value ="/uploads")
 public class ArquivoController {
+
+    @PostMapping(value = "/up")
+    public String uploadArquivo(@RequestParam MultipartFile arquivo, User user){
+
+        return "arquivo show";
+    }
 }
