44cf0b9b3c00d198ba72745de1b41d34255a4c77
Your Namevinicius <viniciusFortti>
2019-07-11 14:58:54 -0300

    (feat): lombok e docker rodando aplicaçao, nao conecta com mongo.

--- ftpserver/src/main/java/desafio/ftp/ftpserver/model/Usuario.java
+++ ftpserver/src/main/java/desafio/ftp/ftpserver/model/Usuario.java
@@ -1,77 +1,20 @@
 package desafio.ftp.ftpserver.model;
 
-import org.hibernate.validator.constraints.br.CPF;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+import lombok.NonNull;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
-//TODO implementar lombok
-@Document(collection = "usuario")
+
+@Data
 public class Usuario {
 
-    @Id
     private Long id;
-    @NotNull
     private String nome;
-    @NotNull
-    @CPF
     private String cpf;
-    @NotNull
-    @Email
     private String email;
-    @NotNull
     private String senha;
-
     private List<Usuario> amigos;
 
-    public List<Usuario> getAmigos() {
-        return amigos;
-    }
 
-    public void setAmigos(List<Usuario> amigos) {
-        this.amigos = amigos;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 }
